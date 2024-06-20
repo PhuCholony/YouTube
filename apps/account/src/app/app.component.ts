8fba27574442d51d34de97ec90e0c8fad7c06b39
Phuchon Chueapong <128043114+PhuCholony@users.noreply.github.com>
2024-06-20 16:54:10 +0700

    refactor all application
    change prefix to ytd
    remove default nx component
    change title related application

--- apps/account/src/app/app.component.ts
+++ apps/account/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet],
+  selector: 'ytd-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- apps/studio/src/app/app.component.ts
+++ apps/studio/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet],
+  selector: 'ytd-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'studio';
+  title = 'account';
 }
